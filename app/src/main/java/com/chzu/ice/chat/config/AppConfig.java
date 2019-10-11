2fd410520c44626b724325927fb467faea05107d
ICE1997 <592092078@qq.com>
2019-10-11 09:21:02 +0800

    update

--- app/src/main/java/com/chzu/ice/chat/config/AppConfig.java
+++ app/src/main/java/com/chzu/ice/chat/config/AppConfig.java
@@ -1,7 +1,7 @@
 package com.chzu.ice.chat.config;
 
 public class AppConfig {
-    public static final String SERVER_DEV = "http://10.200.16.124:18080/api";
+    public static final String SERVER_DEV = "http://192.168.50.71:18080/api";
     public static final String REGISTER_API = SERVER_DEV + "/userAccount/register";
     public static final String LOGIN_API = SERVER_DEV + "/userAccount/login";
     public static final String ADD_FRIEND_API = SERVER_DEV + "/friend/addFriend";
@@ -13,6 +13,6 @@ public class AppConfig {
     public static final String SP_CONFIG_KEY_SIGNED_IN_USER = "signed_in_user";
     public static final String SP_CONFIG_KEY_HAS_SIGNED_IN = "has_signed_in";
     public static final String SP_CONFIG_KEY_IS_FIRST_OPEN = "is_first_open";
-    public static final String SP_CONFIG_KEY_SIGNED_IN_USER_TOPIC = "signed_in_user_topic";
+
 
 }
